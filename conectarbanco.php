1cb09908a527e8422009ac540879da96174cbb40
jonrato <jonatas_ferreira@usp.br>
2023-12-06 18:12:51 -0300

    credenciais de acesso ao banco de dados

--- conectarbanco.php
+++ conectarbanco.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = array(
-    'db_user' => '============',
-    'db_pass' => '===============',
-    'db_name' => '============'
+    'db_user' => 'u756913841_teste',
+    'db_pass' => 'Severino@123',
+    'db_name' => 'u756913841_subwayteste'
 );
 
 ?>
